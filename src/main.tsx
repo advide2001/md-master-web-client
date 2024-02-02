710e36c06e5445d7a8635109ca3c7ec735fc7e94
Adarsh Desai <desaiadarsh22@gmail.com>
2024-02-02 16:44:42 +0530

    refactor: Update entry point to use React Router and ClerkProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+import './index.css'
+import ErrorPage from './pages/ErrorPage'
+import RootPage from './pages/RootPage'
+
+const clerkKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootPage />,
+    errorElement: <ErrorPage />,
+    children: [],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ClerkProvider publishableKey={clerkKey}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </ClerkProvider>
 )
