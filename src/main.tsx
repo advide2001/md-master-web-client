b4ea7a2e352b6e6f9f82fa72fb706f4c7730518f
Adarsh Desai <desaiadarsh22@gmail.com>
2024-02-08 19:52:25 +0530

    chore: update routes with additional page and comments

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { ClerkProvider } from '@clerk/clerk-react'
 import './index.css'
 import ErrorPage from './pages/ErrorPage'
 import RootPage from './pages/RootPage'
+import TestElement from './components/TestElement'
 
 const clerkKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -14,7 +15,12 @@ const router = createBrowserRouter([
     path: '/',
     element: <RootPage />,
     errorElement: <ErrorPage />,
-    children: [],
+    children: [
+      {
+        path: 'prod',
+        element: <TestElement />,
+      },
+    ],
   },
 ])
 
@@ -25,3 +31,13 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </React.StrictMode>
   </ClerkProvider>
 )
+
+// CLERK USER INJECTIONS
+// At the moment, routes that require auth are failing from postman (because of timeout). add a component to the frontend to test this out.
+// Rewrite healthcheck routes to check the health of the server and database connection.
+// use webhooks to trigger a user entry into the db, when a user registers.
+// at the moment the post endpint is not supported. for now just make sure the server accepts the request.
+// Rewrite the functions on the clerk webhok routes to add users as they log in.
+
+// DATABASE SCHEMA REDO
+// Design and implement a new database schema similar to the one overleaf
