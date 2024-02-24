f60efebb0a38504cd67f907c3d167f40c29b2ceb
Adarsh Desai <desaiadarsh22@gmail.com>
2024-02-24 15:21:16 +0530

    chore: Integrate React Query for enhanced data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 
 import './index.css'
 import ErrorPage from './pages/ErrorPage'
@@ -10,6 +11,7 @@ import TestElement from './components/TestElement'
 
 const clerkKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
+const queryClient = new QueryClient()
 const router = createBrowserRouter([
   {
     path: '/',
@@ -25,19 +27,11 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ClerkProvider publishableKey={clerkKey}>
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  </ClerkProvider>
+  <QueryClientProvider client={queryClient}>
+    <ClerkProvider publishableKey={clerkKey}>
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
+    </ClerkProvider>
+  </QueryClientProvider>
 )
-
-// CLERK USER INJECTIONS
-// At the moment, routes that require auth are failing from postman (because of timeout). add a component to the frontend to test this out.
-// Rewrite healthcheck routes to check the health of the server and database connection.
-// use webhooks to trigger a user entry into the db, when a user registers.
-// at the moment the post endpint is not supported. for now just make sure the server accepts the request.
-// Rewrite the functions on the clerk webhok routes to add users as they log in.
-
-// DATABASE SCHEMA REDO
-// Design and implement a new database schema similar to the one overleaf
