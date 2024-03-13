d5437c94112ee570a972637a4dd71c18e5971b83
Adarsh Desai <desaiadarsh22@gmail.com>
2024-03-13 13:24:26 +0530

    refactor: Simplify AuthButton component logic

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -1,4 +1,5 @@
-import { SignInButton, UserButton } from '@clerk/clerk-react'
+import { UserButton } from '@clerk/clerk-react'
+import LogInButton from './LogInButton'
 
 interface Props {
   isSignedIn: boolean | undefined
@@ -9,11 +10,7 @@ const AuthButton = (props: Props) => {
   if (!props.isLoaded) return null
   if (props.isSignedIn) return <UserButton />
 
-  return (
-    <div className="rounded-full bg-accent-color px-4 py-1 font-bold">
-      <SignInButton />
-    </div>
-  )
+  return <LogInButton />
 }
 
 export default AuthButton
