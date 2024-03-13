ffee8cb7e9b781755b33ad1c6b20a1ffadd6f771
Adarsh Desai <desaiadarsh22@gmail.com>
2024-03-13 13:38:42 +0530

    refactor: Update ErrorPage component

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,14 +1,15 @@
-type pageProps = {
-  error?: string
-}
+import { useRouteError } from 'react-router-dom'
 
-const ErrorPage = ({ error }: pageProps) => {
+const ErrorPage = () => {
+  const error = useRouteError()
+
   return (
-    <div className="flex h-[100vh] flex-col items-center justify-center">
+    <div className="flex h-[100vh] flex-col items-center justify-center text-white">
       <h1 className="pb-10 text-4xl font-bold">Oops!</h1>
       <p className="text-xl">Sorry, an unexpected error occurred.</p>
       <p className="font-light">
-        <i>{error || 'Not found'}</i>
+        {/* @ts-ignore */}
+        <i>{error.data}</i>
       </p>
     </div>
   )
