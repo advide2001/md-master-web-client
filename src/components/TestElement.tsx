05fab3dba0361f49e0dc6a0f284c71a2a97993ac
Adarsh Desai <desaiadarsh22@gmail.com>
2024-03-29 20:40:15 +0530

    style: Update TestElement component styling in src/components

--- src/components/TestElement.tsx
+++ src/components/TestElement.tsx
@@ -6,10 +6,16 @@ import {
 
 const TestElement = () => {
   return (
-    <div>
+    <div className="font-semibold text-white">
       <ServerHealthCheck />
       <AuthHealthCheck />
       <DatabaseHealthCheck />
+      <div className="flex gap-10 bg-white">
+        <div className="m-4 h-20 w-32 bg-primary-1"></div>
+        <div className="m-4 h-20 w-32 bg-primary-2"></div>
+        <div className="m-4 h-20 w-32 bg-primary-2"></div>
+        <div className="m-4 h-20 w-32 bg-accent-color"></div>
+      </div>
     </div>
   )
 }
