52e5d696db8f1445c9e58452b8488ea04a2d8eb6
Adarsh Desai <desaiadarsh22@gmail.com>
2024-03-29 20:41:21 +0530

    feat: Add navigation to Projects page in CTAButton component

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom'
 import LogInButton from './LogInButton'
 
 interface Props {
@@ -7,12 +8,14 @@ interface Props {
 
 const CTAButton = (props: Props) => {
   if (!props.isLoaded) return null
+  const navigate = useNavigate()
   if (props.isSignedIn)
     return (
       <div>
         <button
           className="rounded-md bg-accent-color px-3.5 py-2.5 text-base font-semibold text-white hover:bg-accent-color 
                 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-accent-color"
+          onClick={() => navigate('/projects')}
         >
           Go to Docs
         </button>
