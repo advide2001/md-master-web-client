7fc637c57d5ca40b44086498ec4530518efc3153
Adarsh Desai <desaiadarsh22@gmail.com>
2024-03-29 20:38:40 +0530

    feat: Add ProjectPage component to src/pages

---
+++ src/pages/ProjectPage.tsx
@@ -0,0 +1,9 @@
+const ProjectPage = () => {
+  return (
+    <div className="mx-auto max-w-2xl text-center text-white">
+      This is the Projects page
+    </div>
+  )
+}
+
+export default ProjectPage
