71cdb37e7bb8d61444c34eaa8d77c88eb63cbe24
advide2001 <desaiadarsh22@gmail.com>
2024-09-12 08:37:24 +0530

    feat: integrate project fetching with Clerk authentication and React Query

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,7 +1,24 @@
+import { useAuth, useUser } from '@clerk/clerk-react'
+import { useQuery } from '@tanstack/react-query'
+
 const ProjectPage = () => {
+  const { getToken } = useAuth()
+  const template = 'jwt-prod-md-master'
+  const { user } = useUser()
+
+  const { data: markdownDocuments } = useQuery({
+    queryKey: ['markdownDocuments'],
+    queryFn: async () =>
+      fetch(`${import.meta.env.VITE_SERVER_URL}/projects?userId=${user?.id}`, {
+        headers: { Authorization: `Bearer ${await getToken({ template })}` },
+      }).then((response) => response.json()),
+  })
+
+  console.log(markdownDocuments)
+
   return (
     <div className="mx-auto max-w-2xl text-center text-white">
-      This is the Projects page
+      <h1 className="text-3xl font-bold tracking-tight">Project Page</h1>
     </div>
   )
 }
